10b8f09d5faf3030b18520837397170e9428a7f8
Edilson-04 <edilsdondelpiero@gmail.com>
2023-10-28 19:20:40 -0500

    Proyecto y carpetas

---
+++ src/main/java/com/edu/cibertec/DAWII_CL2_GALINDO/DawiiCl2GalindoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.DAWII_CL2_GALINDO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2GalindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2GalindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/DAWII_CL2_GALINDO/DawiiCl2GalindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.DAWII_CL2_GALINDO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2GalindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
